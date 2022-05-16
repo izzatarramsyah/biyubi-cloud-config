56ff287309082fd08737270239e7d7f3c7ff414d
adl.izzata <adl.izzata@xl.co.id>
2022-05-16 19:16:24 +0700

    initial commit

---
+++ src/main/java/com/clinic/config/server/HomeController.java
@@ -0,0 +1,23 @@
+package com.clinic.config.server;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String formattedDate = dateFormat.format(date);
+		return "success connect to config server " + formattedDate;
+	}
+		
+}
